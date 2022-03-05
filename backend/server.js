6804e83696a15e94f6056cdc20838bcf670eaf70
papa-react <makiya112545@gmail.com>
2022-03-05 17:33:22 +0300

    make route and send json response

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+// Load HTTP module
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/api/goals", function (req, res) {
+	res.status(200).json({ name: "hello" });
+});
+
+app.listen(port);
