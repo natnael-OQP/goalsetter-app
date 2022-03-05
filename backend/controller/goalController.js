1246e8623613fe4cc4355b1b5277d0ed655feb38
papa-react <makiya112545@gmail.com>
2022-03-05 22:04:20 +0300

    setup controller

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,18 +1,23 @@
-const getGoal = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+const getGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ name: "get goals" });
-};
+});
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+	if (!req.body?.text) {
+		res.status(400).json({ error: "pleas add text field" });
+	}
 	res.status(200).json({ name: "set goals" });
-};
+});
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ name: `delete goal ${req.params.id}` });
-};
+});
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ name: `update goal ${req.params.id}` });
-};
+});
 
 module.exports = {
 	getGoal,
