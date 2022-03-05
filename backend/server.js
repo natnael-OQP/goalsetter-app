0eb5ff050667ea2f241813e815cd87602b85f630
papa-react <makiya112545@gmail.com>
2022-03-05 22:03:44 +0300

    get json from rew.body

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(express.json());
+// app.use(express.urlencoded());
 
+app.use("/api/goals", require("./routes/goalRoutes"));
 app.listen(port);
