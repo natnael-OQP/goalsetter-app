be23445728ec6f905af6c5d4ffa15d77c930e024
papa-react <makiya112545@gmail.com>
2022-03-06 12:05:55 +0300

    create /api/users  endpoint

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,5 @@ app.use(express.json());
 // app.use(express.urlencoded());
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.listen(port);
