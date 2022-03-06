236c67262a5071fa79b23e896436731dcd9656ba
papa-react <makiya112545@gmail.com>
2022-03-06 10:09:26 +0300

    goals model

---
+++ backend/models/goal.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const goalSchema = new Schema(
+	{
+		text: { type: String, required: true },
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("goal", goalSchema);
