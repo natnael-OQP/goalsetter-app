87fc7eccacf899a3a4a10929dc1576421128e797
papa-react <makiya112545@gmail.com>
2022-03-06 22:39:00 +0300

    delete goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,8 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const Goal = require("../models/goal");
+const User = require("../models/user");
 
 const getGoal = asyncHandler(async (req, res) => {
-	const goals = await Goal.find();
+	const goals = await Goal.find({ user: req.user.id });
 	res.status(200).json(goals);
 });
 
@@ -10,28 +11,52 @@ const setGoal = asyncHandler(async (req, res) => {
 	if (!req.body.text) {
 		res.status(400).json({ message: "pleas add text field" });
 	}
-	const goals = await Goal.create(req.body);
+	const goals = await Goal.create({
+		text: req.body.text,
+		user: req.user.id,
+	});
 	res.status(200).json(goals);
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
-	const id = req.params.id;
-	const goal = await Goal.findById(id);
-	if (!goal.text) {
+	const goal = await Goal.findById(req.params.id);
+	if (!goal) {
 		res.status(400).json({ message: "id not found" });
 	}
-	await Goal.findByIdAndDelete(id);
-	res.status(200).json({ _id: id });
+	const user = await User.findById(req.user.id);
+	// Check for user
+	if (!user) {
+		res.status(400).json({ message: "user not found" });
+	}
+	// Make sure the logged in user matches the goal user
+	if (goal.user !== user._id) {
+		res.status(401).json({ message: "user not authorized" });
+	}
+
+	await goal.remove();
+	res.status(200).json({ _id: req.params.id });
 });
 
 const updateGoal = asyncHandler(async (req, res) => {
 	const id = req.params.id;
-	const updatedId = await Goal.findById(id);
-	if (!updatedId) {
+	const goal = await Goal.findById(id);
+	if (!goal) {
 		res.status(400).json({ message: "id not found" });
 	}
-	const goal = await Goal.findByIdAndUpdate(id, req.body, { new: true });
-	res.status(200).json(goal);
+	const user = await User.findById(req.user.id);
+	// Check for user
+	if (!user) {
+		res.status(400).json({ message: "user not found" });
+	}
+	// Make sure the logged in user matches the goal user
+	if (user.id !== goal.user.toString()) {
+		res.status(400).json({ message: "user not authorized" });
+	}
+
+	const updateGoal = await Goal.findByIdAndUpdate(id, req.body, {
+		new: true,
+	});
+	res.status(200).json(updateGoal);
 });
 
 module.exports = {
