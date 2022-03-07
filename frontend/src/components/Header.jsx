01ad11b5c024a57e96221585ca2d3c9174c23d74
papa-react <makiya112545@gmail.com>
2022-03-07 09:48:07 +0300

    style header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,43 @@
-const Header = () => {
-	return <div>Header</div>;
-};
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+function Header() {
+	const user = false;
+	const navigate = useNavigate();
+	const onLogout = () => {
+		navigate("/");
+	};
+
+	return (
+		<header className="header">
+			<div className="logo">
+				<Link to="/">GoalSetter</Link>
+			</div>
+			<ul>
+				{user ? (
+					<li>
+						<button className="btn" onClick={onLogout}>
+							<FaSignOutAlt /> Logout
+						</button>
+					</li>
+				) : (
+					<>
+						<li>
+							<Link to="/login">
+								<FaSignInAlt /> Login
+							</Link>
+						</li>
+						<li>
+							<Link to="/register">
+								<FaUser /> Register
+							</Link>
+						</li>
+					</>
+				)}
+			</ul>
+		</header>
+	);
+}
 
 export default Header;
