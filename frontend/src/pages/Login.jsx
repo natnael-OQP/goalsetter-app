f6ea1214dd2d234d56bb4826b7d7d2c424e95de2
papa-react <makiya112545@gmail.com>
2022-03-07 10:35:31 +0300

    styled Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,67 @@
-const Login = () => {
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
+function Login() {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+	// get value from input field
+	const { email, password } = formData;
+
+	const onChange = (e) => {
+		setFormData((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
+	console.log(formData);
 	return (
-		<div>
-			<h1>Login</h1>
-		</div>
+		<>
+			<section className="heading">
+				<h1>
+					<FaSignInAlt /> Register
+				</h1>
+				<p>Login and start setting goals</p>
+			</section>
+
+			<section className="form">
+				<form onSubmit={onSubmit}>
+					<div className="form-group">
+						<input
+							type="email"
+							className="form-control"
+							id="email"
+							name="email"
+							value={email}
+							placeholder="Enter your email"
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							className="form-control"
+							id="password"
+							name="password"
+							value={password}
+							placeholder="Enter password"
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<button type="submit" className="btn btn-block">
+							Submit
+						</button>
+					</div>
+				</form>
+			</section>
+		</>
 	);
-};
+}
 
 export default Login;
