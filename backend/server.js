f7da0ddf46468e5c69a0cb5b54b18e243381f906
papa-react <makiya112545@gmail.com>
2022-03-07 18:53:10 +0300

    fix cors problem

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 const dbConnect = require("./database/dbConnect");
+var cors = require("cors");
 const port = process.env.PORT || 8000;
 
 dbConnect();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
