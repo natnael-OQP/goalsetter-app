9b07fb22faf430d109c7676a4f82352babb0d565
papa-react <makiya112545@gmail.com>
2022-03-07 18:56:43 +0300

    login completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { FaSignInAlt } from "react-icons/fa";
 import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import { login, reset } from "../features/auth/authSlice";
 
 function Login() {
 	const [formData, setFormData] = useState({
@@ -9,6 +13,23 @@ function Login() {
 	});
 	// get value from input field
 	const { email, password } = formData;
+
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
+		(state) => state.auth
+	);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+		if (isSuccess || user) {
+			navigate("/");
+		}
+		dispatch(reset());
+	}, [user, isError, isSuccess, message, navigate, dispatch]);
 
 	const onChange = (e) => {
 		setFormData((prev) => ({
@@ -19,17 +40,26 @@ function Login() {
 
 	const onSubmit = (e) => {
 		e.preventDefault();
+		const userData = {
+			email,
+			password,
+		};
+		dispatch(login(userData));
 	};
-	const notify = () => {
-		toast("Default Notification !");
-	};
+
+	if (isLoading) return <Spinner />;
 
 	return (
 		<>
 			<section className="heading">
-				<button onClick={notify}>yes</button>
-				<h1>
-					<FaSignInAlt /> Register
+				<h1
+					style={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+				>
+					<FaSignInAlt /> Login
 				</h1>
 				<p>Login and start setting goals</p>
 			</section>
